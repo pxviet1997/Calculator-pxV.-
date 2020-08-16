0c84f519f9dff8e18b399efe4ab2ebd1fd12b50b
Viet Pham <pxviet1997@gmail.com>
2020-08-16 15:02:56 +1000

    Finished with UI Now to functionalities

--- Calculator (#pxV.)/ViewController.swift
+++ Calculator (#pxV.)/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        resultLabel.text! = "0"
+    }
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        if resultLabel.text! == "0" {
+            resultLabel.text! = ""
+        }
+        resultLabel.text! += sender.titleLabel!.text!
+    }
+    
+    
+    @IBAction func operatorButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func resultButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
 }
 
